7ce56aea413954587081b88aacfc244c8e6ef74f
Gustavo Garcia <gustavogblanco@live.com>
2021-10-09 10:07:25 -0500

    update jwt token

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const app = express();
 //Middleware
 //app.use(); -> Middleware
 app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.get('/api', (request, response) => {
     response.json({
@@ -54,7 +57,7 @@ app.post('/api/login', (request, response) => {
 
     if(request.body.username) {
         if(request.body['username'] === usuario.username && request.body['password'] === usuario.password) {
-            jwt.sign({usuario: usuario}, 'secretKey', (err, token) => {
+            jwt.sign({usuario: usuario}, 'secretKey', {expiresIn: '30s'},(err, token) => {
                 response.json(
                     {token}
                 )
@@ -87,9 +90,33 @@ app.post('/api/login', (request, response) => {
     //jwt.sign({usuario}, 'secretKey');
     });*/
 
-app.post('/api/posts', (request, response) => {
-    response.json({
-        message: 'post creado'
+//funcion que valida el token
+// Authorization: Bearer <token>
+const verifyToken = (request, response, next) => {
+    //rescatamos la propiedad authorization que viaja en el requestHEader desde mi cliente al servidor
+    const bearerHeader = request.headers['authorization'];
+    if(typeof bearerHeader !== 'undefined') {
+        const bearerToken = bearerHeader.split(' ')[1];
+        //['Bearer', <token>];
+        request.token = bearerToken;
+        next();
+    } else {
+        response.sendStatus(403);
+    }
+}
+
+app.post('/api/posts', verifyToken, (request, response) => {
+    //Sintaxis -> jwt.verify(jwtString, llavePublicaOPrivada, [options, callback])
+    jwt.verify(request.token, 'secretKey', (err, auth) => {
+        if(err) {
+            response.sendStatus(500);
+        }
+        else {
+            response.json({
+                message: 'post creado',
+                auth: auth
+            })
+        }
     })
 })
 
