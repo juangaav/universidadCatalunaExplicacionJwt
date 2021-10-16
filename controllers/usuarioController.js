942b2b1e8e26d096519151b85db2af43855015c2
Gustavo Garcia <gustavogblanco@live.com>
2021-10-16 07:25:54 -0500

    update token

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const usuarioModelo = require('../models/Usuario');
 const jwt = require('jsonwebtoken');
 
-const generarToken = (usuario) => {
+const generarToken = (usuario, password) => {
     return new Promise((resolve, reject) => {
-        jwt.sign({ usuario: usuario }, 'secretKey', (err, token) => {
+        jwt.sign({ usuario, password }, 'secretKey', (err, token) => {
             if(err) {
                 reject(err)
             } else {
@@ -15,27 +15,30 @@ const generarToken = (usuario) => {
     })
 }
 
+//Callback - Es una funcion dentro de una funcion, primero se ekecuta la funcion
+//donde envio mi callback como parametro y despues se ejecuta el callback.
+
+//Promesas - Nos retorma un resolve() y reject()
+
 //Crea token
 module.exports.token = (request, response) => {
-    console.log('GETALL usuarios');
+    console.log('Token usuario');
     console.log(request.body);
     //find({query}, callback) -> query que hacemos para buscar informacion, como en este caso es 
     //vacio, nos trae todo la informacion
     usuarioModelo.find({username: request.body.username, password: request.body.password}, (err, data) => {
         if (err) {
             response.send(500).json({
-                message: 'Error a ejecutar el metodo findAll()'
+                message: 'Error en la autenticacion'
             });
         } else {
             if (data.length == 0) {
-                console.log('acceso desautorizado');
                 response.sendStatus(403);
             } else {
-                generarToken(request.body.username).then(token => {
-                    console.log(token);
+                generarToken(request.body.username, request.body.password).then(token => {
                     response.status(200).send({token});
                 }).catch(error => {
-                    console.error(error);
+                    response.status(500).json({error: 'Intentelo mas tarde'});
                 });
             }
         }
